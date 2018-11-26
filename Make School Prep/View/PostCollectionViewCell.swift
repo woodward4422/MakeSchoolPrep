4cd6b208400b1bc1b12c3872deab90472932c1a3
noah <noahwoodward2244@gmail.com>
2018-11-26 12:42:37 -0500

    Added most of the collection view starter

---
+++ Make School Prep/View/PostCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  PostCollectionViewCell.swift
+//  Make School Prep
+//
+//  Created by Noah Woodward on 11/25/18.
+//  Copyright © 2018 Noah Woodward. All rights reserved.
+//
+
+import UIKit
+
+class PostCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var categoryLabel: UILabel!
+    
+    
+
+    
+}
